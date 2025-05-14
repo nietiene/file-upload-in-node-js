8038b691d0f5146ff0f966879756968bb62ce695
nietiene <niyomugaboetiene53@gmail.com>
2025-05-14 16:19:54 -0700

    the commit of file upload

---
+++ imgae-upload-validation/app.js
@@ -0,0 +1,53 @@
+const { error } = require("console");
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const App = express();
+App.use("view engine", "ejs"); 
+App.use(express.static("uploads")); 
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "uploads");
+    },
+    filename: function(req, file, cb) {
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+
+const filterImage = function(req, file, cb) {
+    const allowedType = /jpeg|jpg|png/;
+    const extname = allowedType.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = allowedType.test(file.mimetype);
+
+    if (mimetype && extname) {
+        return cb(null, true);
+    } else {
+        cb(null, Error("Only .jpg, .jpeg, .png images are allowed"))
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limits: {fileSize: 2 * 1024 * 1024},
+    fileFilter: fileFilter
+});
+
+App.get('/', (req, res) => {
+    res.render('upload', { error: null });
+});
+
+App.post('/upload', (req, res) => {
+    upload.single('myfile')(req, res, function(err) {
+        if (err) {
+            return res.render("upload", {error: err.message});
+        }
+        if (!req.file) {
+            return res.render('upload', { error:  "Please select a file" });
+        }
+        res.render("sucess", { message: req.file.filename });
+    });
+});
+
+App.listen(3000, () => console.log("http://localhost:3000"));
